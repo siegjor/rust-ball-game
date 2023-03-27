6b767f3442678753d81f8050ea53af24bb898219
Gabriel <bristotgl@gmail.com>
2023-03-27 10:20:10 -0300

    adds startup system and system for querying races

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_system(hello_world).run();
+    App::new()
+        .add_startup_system(setup)
+        .add_system(print_names)
+        .run();
 }
 
-fn hello_world() {
-    println!("Hello world!");
+pub fn setup(mut commands: Commands) {
+    commands.spawn(Person {
+        race: "Elf".to_string(),
+    });
+    commands.spawn(Person {
+        race: "Dwarf".to_string(),
+    });
+    commands.spawn(Person {
+        race: "Halfling".to_string(),
+    });
+}
+
+pub fn print_names(person_query: Query<&Person>) {
+    for person in person_query.iter() {
+        println!("Race: {}", person.race);
+    }
+}
+
+#[derive(Component)]
+pub struct Person {
+    pub race: String,
 }
